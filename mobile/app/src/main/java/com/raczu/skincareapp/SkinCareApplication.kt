382b6b2e07093446da379b54a8bee2dd5e5a974d
raczu <raczuekk@gmail.com>
2026-01-15 22:33:49 +0100

    feat: show push notification when app is foreground

--- mobile/app/src/main/java/com/raczu/skincareapp/SkinCareApplication.kt
+++ mobile/app/src/main/java/com/raczu/skincareapp/SkinCareApplication.kt
@@ -1,10 +1,7 @@
 package com.raczu.skincareapp
 
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.os.Build
+import com.raczu.skincareapp.data.local.notifications.AppNotificationManager
 import com.raczu.skincareapp.di.AppContainer
 
 class SkinCareApplication : Application() {
@@ -13,22 +10,7 @@ class SkinCareApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         container = AppContainer(this)
-        createNotificationChannel()
-    }
-
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
-            val channelId = getString(R.string.default_notification_channel_id)
-            val name = getString(R.string.default_notification_channel_name)
-            val descriptionText = getString(R.string.default_notification_channel_description)
-
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(channelId, name, importance).apply {
-                description = descriptionText
-            }
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
+        val notificationManager = AppNotificationManager(this)
+        notificationManager.createNotificationChannel()
     }
 }
--- mobile/app/src/main/java/com/raczu/skincareapp/data/remote/service/AppMessagingService.kt
+++ mobile/app/src/main/java/com/raczu/skincareapp/data/remote/service/AppMessagingService.kt
@@ -1,17 +1,24 @@
 package com.raczu.skincareapp.data.remote.service
 
+import android.content.Intent
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.raczu.skincareapp.MainActivity
+import com.raczu.skincareapp.R
 import com.raczu.skincareapp.SkinCareApplication
+import com.raczu.skincareapp.data.local.notifications.AppNotificationManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import kotlin.getValue
 
 class AppMessagingService : FirebaseMessagingService() {
     private val serviceScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    private val appNotificationManager by lazy { AppNotificationManager(applicationContext) }
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
@@ -20,6 +27,13 @@ class AppMessagingService : FirebaseMessagingService() {
         serviceScope.launch {
             val isLoggedIn = container.tokenManager.isUserLoggedIn.first()
             if (isLoggedIn) sendRegistrationToServer(token)
+        }
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+        remoteMessage.notification?.let {
+            appNotificationManager.showNotification(it.title, it.body)
         }
     }
 
